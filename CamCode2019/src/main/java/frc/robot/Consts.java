3fa8c4d9df3bd4e3f4c8801a1913aee48621814a
camclarke64 <camsyta81600@gmail.com>
2019-01-12 14:49:18 -0600

    Cam Upload

---
+++ CamCode2019/src/main/java/frc/robot/Consts.java
@@ -0,0 +1,23 @@
+package frc.robot;
+
+public class Consts {
+
+    public static final int xBoxComPort = 0;
+	// DriveTrain
+
+	/*public static final int leftOne = 1;
+	public static final int leftTwo = 2;
+	public static final int leftThree = 3;
+	public static final int rightFour = 4;
+	public static final int rightFive = 5;
+	public static final int rightSix = 6;*/
+
+	public static final int fLeftMotor = 0;
+	public static final int bLeftMotor = 1;
+	public static final int fRightMotor = 2;
+	public static final int bRightMotor = 3;
+
+	public static final int millisecondsPerSecond = 1000;
+	public static final int timeOutMs = 10;
+	public static final double wheelRadiusInch = 3;
+}
